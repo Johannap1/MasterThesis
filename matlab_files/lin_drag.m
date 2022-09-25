19a92cbc235e16c60dce2bc3afb9133a9556a5fb
Johanna <j.s.probst@student.tudelft.nl>
2022-09-25 16:46:26 +0200

    Remove matlab files

--- matlab_files/lin_drag.m
+++
@@ -1,6 +0,0 @@
-function [A,B,C,D] = lin_drag(par,T,aux)
-A = -par(1);
-B = -aux;
-C = 1;
-D = 0;
-end
--- matlab_files/nl_drag.m
+++
@@ -1,4 +0,0 @@
-function [dx, y] = nl_drag(t,x,u,par,varargin)
-dx = -par*x + 9.81*-tan(u);
-y = x;
-end
